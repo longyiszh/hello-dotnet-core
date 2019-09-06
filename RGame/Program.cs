157f5f7e84cdace4ec35b612052d5ca42bf75c22
longyiszh <longyiszh@gmail.com>
2019-09-06 16:23:48 -0400

    csharp delegate

---
+++ RGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayRGames();
+        }
+
+
+        static void PlayRGames()
+        {
+            List<RGames> RGamess = new List<RGames>();
+
+            RGamess.Add(new RGames() { ID = 100, name = "tony", health = 100.0, maxHealth = 100.0 });
+            RGamess.Add(new RGames() { ID = 101, name = "riba", health = 60.0, maxHealth = 100.0 });
+            RGamess.Add(new RGames() { ID = 102, name = "phor", health = 20.0, maxHealth = 100.0 });
+            RGamess.Add(new RGames() { ID = 103, name = "xeno", health = 1.0, maxHealth = 100.0 });
+
+            QSTRestoreHealth qstRestoreHealth1 = new QSTRestoreHealth(questionableRH1);
+            QSTRestoreHealth qstRestoreHealth2 = new QSTRestoreHealth(questionableRH1);
+
+            foreach (RGames RGames in RGamess)
+            {
+                RGames.restoreHealth(20.5, qstRestoreHealth2);
+                string info = (RGames.health <= 0 ?
+                    RGames.name + " is like X_X with remaining HP " + RGames.health :
+                    RGames.name + " is still alive with remaining HP " + RGames.health
+                );
+                Console.WriteLine(info);
+
+            }
+        }
+
+        public static double questionableRH1(double health, double maxHealth)
+        {
+            return health * (maxHealth - health);
+        }
+
+        public static double questionableRH2(double health, double maxHealth)
+        {
+            return health - (maxHealth * health);
+        }
+
+
+
+    }
+}
---
+++ RGame/RGames.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RGame
+{
+    public delegate double QSTRestoreHealth(double health, double maxHealth);
+    public class RGames
+    {
+        public int ID;
+        public string name;
+        public double health;
+        public double maxHealth;
+
+        //List<string> effects = new List<string> { "" };
+
+        public void restoreHealth(double val, QSTRestoreHealth rsHealth)
+        {
+            health += val;
+            health *= rsHealth(health, maxHealth);
+
+        }
+
+        public void restoreHealth(double val)
+        {
+            health += val;
+        }
+
+    }
+}
