281cfbdadfa80acb5b0993d6a63e450e35085e29
longyiszh <longyiszh@gmail.com>
2019-09-07 11:15:35 -0400

    serving static files, customizing middleware, dev ex page

--- employee/Startup.cs
+++ employee/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace employee
 {
@@ -26,13 +25,38 @@ namespace employee
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-
+            // handles exception, plug in as earlier as possible
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // customize dev exception page
+                DeveloperExceptionPageOptions devExOptions = new DeveloperExceptionPageOptions();
+                devExOptions.SourceCodeLineCount = 10;
+                app.UseDeveloperExceptionPage(devExOptions);
             }
+
+            #region customize default html page method 1
+            //// customize default html page
+            //DefaultFilesOptions dfOptions = new DefaultFilesOptions();
+            //dfOptions.DefaultFileNames.Clear();
+            //dfOptions.DefaultFileNames.Add("fancy.html");
+
+            //// order of these two 'use's is very important
+            //// UseDefaultFiles() must present ahead of UseStaticFiles()
+            //app.UseDefaultFiles(options);
+            //app.UseStaticFiles();
+
+            #endregion
+
+            #region customize default html page method 2
+            //FileServerOptions fsOptions = new FileServerOptions();
+            //fsOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fsOptions.DefaultFilesOptions.DefaultFileNames.Add("fancy.html");
+            //app.UseFileServer(fsOptions);
+            #endregion
+
+            app.UseFileServer();
 
             //app.Use(async (context, next) =>
             //{
@@ -51,11 +75,12 @@ namespace employee
             // Terminal Middleware: Pipeline starts to reverse
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync(_config["xmj"]);
-                logger.LogInformation("M3-req");
-                await context.Response.WriteAsync("M3-req\n");
-                logger.LogInformation("M3-res");
-                await context.Response.WriteAsync("M3-res\n");
+                //await context.Response.WriteAsync(_config["xmj"]);
+                //logger.LogInformation("M3-req");
+                //await context.Response.WriteAsync("M3-req\n");
+                //logger.LogInformation("M3-res");
+                //await context.Response.WriteAsync("M3-res\n");
+                throw new Exception("Lan lo");
 
             });
         }
