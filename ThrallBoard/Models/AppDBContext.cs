755e85b1228b3e68ed55751744eb7bea10dfb4ed
longyiszh <longyiszh@gmail.com>
2019-12-16 19:54:27 -0500

    Seed the employee Database

--- ThrallBoard/Models/AppDBContext.cs
+++ ThrallBoard/Models/AppDBContext.cs
@@ -15,5 +15,10 @@ namespace ThrallBoard.Models
         }
 
         public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
     }
 }
---
+++ ThrallBoard/Models/EmployeeSeed.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThrallBoard.Models
+{
+    public static class EmployeeSeed
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+
+                new Employee
+                {
+                    ID = 1,
+                    Name = "funnyKitt",
+                    Avatar = "avatar-funnyKitt.jpg",
+                    Email = "funnyKitt@rmvb.cct",
+                    Department = Dept.Comedy
+                },
+
+                new Employee
+                {
+                    ID = 2,
+                    Name = "bigRuaDog",
+                    Avatar = "brd.jpg",
+                    Email = "puppyRua@cwrp.com",
+                    Department = Dept.IT
+                }
+
+            );
+        }
+    }
+}
