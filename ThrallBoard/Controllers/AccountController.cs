5f33aba8ceb4b149e0cac2860c425fb237630a60
longyiszh <longyiszh@gmail.com>
2020-05-03 15:41:45 -0400

    Register user

--- ThrallBoard/Controllers/AccountController.cs
+++ ThrallBoard/Controllers/AccountController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ThrallBoard.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,49 @@ namespace ThrallBoard.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager
+        )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("home", "index");
+                }
+
+                foreach((var error, int index) in result.Errors.Select((error, index) => (error, index)))
+                {
+                    ModelState.AddModelError(index.ToString(), error.Description);
+                }
+
+            }
+            return View(model);
         }
     }
 }
