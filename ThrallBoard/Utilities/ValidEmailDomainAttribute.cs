e632644c7b752f1b86e6d065adfcfa0e058fb125
longyiszh <longyiszh@gmail.com>
2021-01-09 18:23:56 -0500

    custom attribute to validate Email Domain

---
+++ ThrallBoard/Utilities/ValidEmailDomainAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThrallBoard.Utilities
+{
+    public class ValidEmailDomainAttribute: ValidationAttribute
+    {
+        private readonly string allowedDomain;
+
+        public ValidEmailDomainAttribute(string allowedDomain)
+        {
+            this.allowedDomain = allowedDomain;
+        }
+
+        public override bool IsValid(object value)
+        {
+            string[] emailSegments = value.ToString().Split('@');
+            return emailSegments[1].ToLower() == allowedDomain.ToLower();
+        }
+    }
+}
--- ThrallBoard/ViewModels/RegisterViewModel.cs
+++ ThrallBoard/ViewModels/RegisterViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ThrallBoard.Utilities;
 
 namespace ThrallBoard.ViewModels
 {
@@ -13,6 +14,7 @@ namespace ThrallBoard.ViewModels
         [EmailAddress]
         [Display(Name = "Email Address")]
         [Remote(controller: "Account", action: "IsEmailInUse")]
+        [ValidEmailDomain(allowedDomain: "jjj.com", ErrorMessage = "Error! This site has been hacked and you can only register with domain jjj.com")]
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
